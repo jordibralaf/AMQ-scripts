3ef861be441402cf68461e330c4617ecda828cf9
jordibralaf <60915799+jordibralaf@users.noreply.github.com>
2020-05-12 13:29:13 +0200

    Add files via upload

---
+++ amqmododejuego.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         AMQ Busca el tonto
+// @namespace    https://github.com/jordibralaf/AMQ-scripts
+// @version      1.0
+// @description  Busca una persona aleatoria en la sala del amq y dice que es tonta
+// @author       Joordi25
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// @require      https://github.com/jordibralaf/AMQ-scripts/blob/master/amqbuscatonto.js
+// ==/UserScript==
+if (!window.setupDocumentDone) return;
+
+let modo = [];
+modo = {openings, endings, inserts};
+
+
+let commandListener = new Listener("Game Chat Message", (payload) => {
+    if (payload.sender === selfName && payload.message.startsWith("/modo")) {
+        if (lobby.inLobby) {
+            let message = "";
+            sendChatMessage("Se está decidiendo el modo de juego");
+
+            shuffle(modo);
+
+
+                message += "Se jugarán " + (modo[1]);
+
+                sendChatMessage(message);
+                message = "";
+
+
+            modo = [];
+        }
+        else {
+            gameChat.systemMessage("Tienes que estar en la lobby para ejecutar este comando");
+        }
+    }
+});
+
+
+function shuffle(array) {
+    let counter = array.length;
+    while (counter > 0) {
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
+
+function sendChatMessage(message) {
+    gameChat.$chatInputField.val(message);
+    gameChat.sendMessage();
+}
+
+commandListener.bindListener();
+
+AMQ_addScriptData({
+    name: "Buscando al tonto",
+    author: "Joordi25",
+    description: `
+        <p>Este script sirve para ver quien es el tonto de la sala</p>
+        <p>Escribe /tonto y elige alguien aleatorio</p>
+    `
+})
