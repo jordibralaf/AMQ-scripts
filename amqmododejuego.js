a4fe87b631314daaad76dfba0cf4d9c037d4c1fe
jordibralaf <60915799+jordibralaf@users.noreply.github.com>
2020-05-13 11:07:22 +0200

    Update amqmododejuego.js

--- amqmododejuego.js
+++ amqmododejuego.js
@@ -9,25 +9,43 @@
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqWindows.js
 // ==/UserScript==
-
 if (!window.setupDocumentDone) return;
 
-
-
+let modo = ["openings", "endings", "inserts", "openings y endings", "openings e inserts", "endings e inserts", "openings, endings e inserts"];
 
 let commandListener = new Listener("Game Chat Message", (payload) => {
-    if (payload.sender === selfName && payload.message.startsWith("/facha")) {
+    if (payload.sender === selfName && payload.message.startsWith("/modo")) {
+        if (lobby.inLobby) {
             let message = "";
-            sendChatMessage("Buscando fachas...");
+            sendChatMessage("Se está decidiendo el modo de juego...");
 
 
-                message += "solo hay un facha y ese es @Jabro";
+            shuffle(modo);
+            let sec = segundosRandom(5,20);
+            let modo_aleatorio = shuffle(modo);
+
+                message += "se jugarán " + (modo_aleatorio) + " con " + sec + " segundos.";
 
                 sendChatMessage(message);
                 message = "";
+
         }
+        else {
+            gameChat.systemMessage("Tienes que estar en la lobby para ejecutar este comando");
+        }
+    }
 });
 
+
+function shuffle(array) {
+    return array[Math.floor(Math.random() * array.length)];
+    }
+
+
+function segundosRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+
+}
 
 function sendChatMessage(message) {
     gameChat.$chatInputField.val(message);
@@ -37,9 +55,9 @@ function sendChatMessage(message) {
 commandListener.bindListener();
 
 AMQ_addScriptData({
-    name: "facha",
-    author: "Joordi25",
+    name: "Modo aleatorio",
+    author: "Joordi25 & TheSpecialConan",
     description: `
-        <p>Para generar aleatoriamente las canciones y los segundos utiliza el comando /modo</p>
+        <p>Poniendo el comando /modo se elegirá aleatoriamente un modo de jueg y los segundos con los que jugar.</p>
     `
-})
+});
